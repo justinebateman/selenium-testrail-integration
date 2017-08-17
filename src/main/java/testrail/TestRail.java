e37d9143e74a117611d1aeba9c6b9cacd5bf2561
Justine Bateman <justinebateman@outlook.com>
2017-08-17 16:30:20 +0100

    Add TestRail annotation to allow us to link TestNG tests to TestRail test cases

---
+++ src/main/java/testrail/TestRail.java
@@ -0,0 +1,14 @@
+package testrail;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD) //can use in method only.
+public @interface TestRail
+{
+    //TestRail Test Case Id
+    int[] testCaseId() default 0;
+}
